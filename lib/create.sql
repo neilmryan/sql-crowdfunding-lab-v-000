7de60768a722b1c7545782ba9928123caa4839b0
Neil Ryan <neilmarkoliverryan@gmail.com>
2020-03-09 17:39:38 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+--A project has a title, a category, a funding goal, a start date, and an end date. 
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
+
+-- A user has a name and an age
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+
+-- A pledge has an amount. It belongs to a user, and it also belongs to a project.
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
